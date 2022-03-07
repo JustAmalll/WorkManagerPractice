516be15d9a341e7566140e8c1da307b563208103
Just_Amalll <amalkrasavchik@gmail.com>
2022-03-07 15:52:34 +0600

    Initial commit

---
+++ app/src/main/java/dev/amal/workmanager/MainActivity.kt
@@ -0,0 +1,28 @@
+package dev.amal.workmanager
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import dev.amal.workmanager.ui.theme.WorkManagerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WorkManagerTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                }
+            }
+        }
+    }
+}
